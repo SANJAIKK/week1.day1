9897114bf9165440eb574d56d710b518146e241d
SANJAIKK <98405366+SANJAIKK@users.noreply.github.com>
2022-01-25 20:10:45 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Car {
+	
+	//Primitive data type
+	
+	byte regnNumber = 007;
+	short seatingCapacity = 7;
+	int insuranceNumber = 987465321;
+	long chassisNumber = 456965478L;  
+	float fuelCapacity = 45-60.5F;
+	double engineoilCapacity = 03.00001;
+	boolean petrolEngine = true;
+	char varient = 'Z';
+	 
+	//Non-Primitive data type
+	
+	String name = "Maruti Suzki Ertiga";
+	
+
+}
